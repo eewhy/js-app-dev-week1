cf40cd441ab4a5a18dedec14484eb8e1ef0f69fe
ey <eyy0221@gmail.com>
2018-10-02 20:02:15 -0400

    More string types

--- variables.js
+++ variables.js
@@ -8,6 +8,42 @@ var declaredVar;
 
 console.log(declaredVar);
 
-declaredVar = 'I\'m declared'
+declaredVar = 'I\'m declared';
 
 console.log(declaredVar);
+
+const myConstant = 'I can\'t change';
+
+console.log(myConstant);
+
+/*
+myConstant = 'I want to change';
+
+console.log(myConstant);
+*/
+
+//this is comment
+
+let myLet = 'I can be changed!';
+
+console.log(myLet);
+
+myLet = 'I\'m changed :)';
+
+console.log(myLet);
+
+const myStr = 'I am a string'
+
+console.log(myStr);
+
+const doubleQuateStr = "I have double quotes";
+
+console.log(doubleQuateStr);
+
+const myLife = "It's my life";
+
+console.log(myLife);
+
+const templateStr = `""''${myLife}`
+
+console.log(templateStr);
