36739d80c5d3aeb2bef2f33c675fcef7d8a66cce
ey <eyy0221@gmail.com>
2018-10-02 20:36:17 -0400

    working typles

---
+++ type.js
@@ -0,0 +1,13 @@
+let myVar = 'this is my string';
+
+console.log(typeof myVar);
+
+myVar = 2;
+
+console.log(typeof myVar);
+
+const amountRaisedSoFar = 1000;
+
+const newDonation = '50';
+
+let nothing;
